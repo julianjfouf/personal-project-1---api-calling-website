e2cec1bc271440165d45a2dc2daa92885cfdc392
julianjfouf <juliancoding30@gmail.com>
2022-07-11 14:56:36 -0700

    updated website

--- search.js
+++ search.js
@@ -1,5 +1,3 @@
-const movieListEl = document.querySelector(".movie__list");
-
 async function main() {
   const id = localStorage.getItem("id");
   const movie = await fetch(
@@ -8,18 +6,16 @@ async function main() {
   const movieData = await movie.json();
   console.log(movieData);
 
-  movieListEl.innerHTML = movieData.Search.map(
-    (movie) => `
-  <li class="movie__links">
+  movieListEl.innerHTML =
+  `<li class="movie__links">
         <img
           class="movie__img"
-          src="${movie.Poster}"
+          src="${movieData.Poster}"
           alt=""
         />
-        <h4 class="movie__title">${movie.Title}</h4>
-        <p class="movie__rating">${movie.Year}</p>
+        <h4 class="movie__title">${movieData.Title}</h4>
+        <p class="movie__rating">${movieData.Year}</p>
       </li>`
-  ).join("");
 }
 
 main();
